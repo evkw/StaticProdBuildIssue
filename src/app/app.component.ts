d44b84473d950d0e0e02130cef5224c590400174
Evan Wallace <ewallace@berkleyre.com.au>
2017-02-24 14:53:16 +1100

    Basic example of issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  public formGroup: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.formGroup = fb.group({
+      id: ['1234'],
+      name: ['This is a test name']
+    });
+  }
 }
---
+++ src/app/shared/generic-input/generic-input.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { AbstractValueAccessor } from '../abstract-value-accessor';
+import { makeValueProvider } from '../make-value-provider';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: 'generic-input.component.html',
+  providers: [makeValueProvider(GenericInputComponent)],
+})
+export class GenericInputComponent extends AbstractValueAccessor {
+  @Input('label') label: string;
+  @Input('placeholder') placeholder: string;
+}
